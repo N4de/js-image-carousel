1e0b7e433031a68ab660d8c3f4a6605b65d0c8c1
Jari Annala <Jari Annala>
2017-05-04 17:52:17 +0300

    add things and stuff

--- carousel.js
+++ carousel.js
@@ -1,5 +1,46 @@
+$(document).ready(function(){
 
 
+	$( "#arrow_next" ).on( "click", function() {
+		      carousel.nextSlide();
+		});
 
+	$( "#arrow_prev" ).on( "click", function() {
+	      carousel.prevSlide();
+	});
 
+});
+
+	const carousel = {
+		
+		count: 0,
+		slides: 3,
+
+		nextSlide: function() {
+			if (carousel.count === (carousel.slides - 1)) {
+				carousel.count = 0;
+			}
+			else {
+				carousel.count++;
+			}
+
+			$( ".effect" ).css('transform', 'translateX(' + (-100 * carousel.count) + '%)');
+			console.log("NextSlide");
+			return carousel.count;
+		},
+
+		prevSlide: function() {
+			if (carousel.count === 0) {
+				carousel.count = (carousel.slides - 1);
+			}
+			else {
+				carousel.count--;
+			}
+
+			$( ".effect" ).css('transform', 'translateX(' + (-100 * carousel.count) + '%)');
+			console.log("PreviousSlide");
+			return carousel.count;
+		},
+
+	};
 

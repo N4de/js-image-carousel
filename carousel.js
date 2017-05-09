38942d3f2d23b4a21c5d4d2ab9a3b6d3304bd2fd
Jari Annala <Jari Annala>
2017-05-09 12:02:35 +0300

    add indicator

--- carousel.js
+++ carousel.js
@@ -1,12 +1,32 @@
 $(document).ready(function(){
 
 
-	$( "#arrow_next" ).on( "click", function() {
-		      carousel.nextSlide();
+	var start = setInterval(function() {carousel.nextSlide();},10000);
+
+	$( "#arrow-next" ).on( "click", function() {
+		    carousel.nextSlide();
 		});
 
-	$( "#arrow_prev" ).on( "click", function() {
-	      carousel.prevSlide();
+	$( "#arrow-prev" ).on( "click", function() {
+	    carousel.prevSlide();
+	});
+
+	$( ".ball-0" ).on( "click", function() {
+		carousel.destroyIndicator();
+		carousel.count = 0;
+	    carousel.moveSlide();
+	});
+
+	$( ".ball-1" ).on( "click", function() {
+		carousel.destroyIndicator();
+		carousel.count = 1;
+	    carousel.moveSlide();
+	});
+
+	$( ".ball-2" ).on( "click", function() {
+		carousel.destroyIndicator();
+		carousel.count = 2;
+	    carousel.moveSlide();
 	});
 
 });
@@ -17,6 +37,9 @@ $(document).ready(function(){
 		slides: 3,
 
 		nextSlide: function() {
+
+			carousel.destroyIndicator();
+
 			if (carousel.count === (carousel.slides - 1)) {
 				carousel.count = 0;
 			}
@@ -24,23 +47,37 @@ $(document).ready(function(){
 				carousel.count++;
 			}
 
-			$( ".effect" ).css('transform', 'translateX(' + (-100 * carousel.count) + '%)');
-			console.log("NextSlide");
+			carousel.moveSlide();
 			return carousel.count;
 		},
 
 		prevSlide: function() {
+
+			carousel.destroyIndicator();
+
 			if (carousel.count === 0) {
 				carousel.count = (carousel.slides - 1);
 			}
+
 			else {
 				carousel.count--;
 			}
 
-			$( ".effect" ).css('transform', 'translateX(' + (-100 * carousel.count) + '%)');
-			console.log("PreviousSlide");
+			carousel.moveSlide();
 			return carousel.count;
 		},
 
+		moveSlide: function() {
+			$( ".effect" ).css('transform', 'translateX(' + (-100 * carousel.count) + '%)');
+			carousel.createIndicator();
+		},
+
+		destroyIndicator: function() {
+			$( ".ball-" + carousel.count ).removeClass("active");
+		},
+
+		createIndicator: function() {
+			$( ".ball-" + carousel.count ).addClass("active");
+		},
 	};
 
